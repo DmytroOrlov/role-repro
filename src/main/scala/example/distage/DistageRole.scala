d6199b16886935cf94f6a93a8627ce5b7b37132b
Dmytro Orlov <orlov.dm@gmail.com>
2023-03-30 16:24:12 +0200

    17

--- src/main/scala/example/distage/DistageRole.scala
+++ src/main/scala/example/distage/DistageRole.scala
@@ -1,34 +1,34 @@
 package example.distage
 
 import distage.plugins.{PluginConfig, PluginDef}
-import distage.Lifecycle
+import distage.{Lifecycle, Lifecycle3}
 import izumi.distage.roles.RoleAppMain
 import izumi.distage.roles.model.definition.RoleModuleDef
 import izumi.distage.roles.model.{RoleDescriptor, RoleService}
-import izumi.functional.bio.Applicative2
+import izumi.functional.bio.Applicative3
 import izumi.fundamentals.platform.IzPlatform
-import izumi.fundamentals.platform.cli.model.raw.{RawEntrypointParams, RawRoleParams}
-import logstage.LogIO2
-import zio.IO
+import izumi.fundamentals.platform.cli.model.raw.RawEntrypointParams
+import logstage.LogIO3
+import zio.*
 
 import scala.annotation.unused
 
-trait TestApi
+trait TestApi[F[_, _, _]]
 
 object TestPlugin extends PluginDef with RoleModuleDef {
-  makeRole[TestRole[IO]]
+  makeRole[TestRole[ZIO]]
 
-  make[TestApi].fromValue(new TestApi {})
+  make[TestApi[ZIO]].fromValue(new TestApi[ZIO] {})
 }
 
-final class TestRole[F[+_, +_]: Applicative2](
-    @unused testApi: TestApi,
-    log: LogIO2[F],
-) extends RoleService[F[Throwable, _]] {
+final class TestRole[F[-_, +_, +_]: Applicative3](
+    @unused testApi: TestApi[F],
+    log: LogIO3[F],
+) extends RoleService[F[Any, Throwable, _]] {
   override def start(
       roleParameters: RawEntrypointParams,
       freeArgs: Vector[String],
-  ): Lifecycle[F[Throwable, _], Unit] = {
+  ): Lifecycle3[F, Any, Throwable, Unit] = {
     Lifecycle.liftF(log.info("Test role started!"))
   }
 }
@@ -38,7 +38,7 @@ object TestRole extends RoleDescriptor {
 }
 
 object TestLauncher extends RoleAppMain.LauncherBIO2[IO] {
-  override def pluginConfig: PluginConfig =
+  def pluginConfig =
     if (IzPlatform.isGraalNativeImage) PluginConfig.const(List(TestPlugin))
     else PluginConfig.cached(pluginsPackage = "example.distage")
 }
